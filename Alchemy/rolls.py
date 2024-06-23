d6fa04e68157544e891e8f076ad31d7d5abc3fdd
Mifikus <mifikus322@gmail.com>
2024-06-23 16:43:07 +0300

    Фикс

--- Alchemy/rolls.py
+++ Alchemy/rolls.py
@@ -760,8 +760,8 @@ class GoogleSheets:
 
 class DataBase:
     def __init__(self):
-        #self.host = '192.168.0.10'
-        self.host = '127.0.0.1'
+        self.host = '192.168.0.10'
+        #self.host = '127.0.0.1'
         self.user = 'root'
         self.password = 'BigBot'
         #self.password = 'root'
